a0df2b4aca192bfc1e4f01f00cc6f4d9e9e1065e
wucong-github <3288448426@qq.com>
2020-03-24 16:14:25 +0800

    7001端口测试完成，服务的注册与发现

--- microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/DeptConsumer80_App.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/DeptConsumer80_App.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 public class DeptConsumer80_App {
 
     public static void main(String[] args) {
