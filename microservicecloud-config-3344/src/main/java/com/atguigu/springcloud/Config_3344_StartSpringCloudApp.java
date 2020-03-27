82f0dfbfcf4cf96393d79dc267e6257b0637b2ea
wucong-github <3288448426@qq.com>
2020-03-27 18:24:33 +0800

    âConfig实战

---
+++ microservicecloud-config-3344/src/main/java/com/atguigu/springcloud/Config_3344_StartSpringCloudApp.java
@@ -0,0 +1,21 @@
+package com.atguigu.springcloud;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
+/**
+ *  @EnableConfigServer  开启Config服务端 服务端可以访问远程库
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class Config_3344_StartSpringCloudApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Config_3344_StartSpringCloudApp.class, args);
+    }
+
+}
